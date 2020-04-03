3e14fd1a1102bbe5ef98d9e51cd43ac727c320b2
Gary Yang <ser.ygr@gmail.com>
2020-04-02 23:42:05 -0700

    Add hirb gem and create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: 'gary.yang', password: 'foobar')
+User.create(username: 'sherry.dai', password: 'foobar')
+User.create(username: 'shishi.zhang', password: 'foobar')
